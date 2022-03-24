c93a7903dd1a025451a7b3cb212446119c1c2bf1
chiazor007 <chiazor007@gmail.com>
2022-03-24 19:40:49 +0000

    My forst change

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 // https://www.npmjs.com/package/express
 const express = require('express');
 
+// I added this
+
 // converts content in the request into parameter req.body
 // https://www.npmjs.com/package/body-parser
 const bodyParser = require('body-parser');
